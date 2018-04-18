fa5a1d0e23f35dc2100f0ad1fb7f3fc395fb3c09
mostwk <moskvichev.95@gmail.com>
2018-04-19 02:43:06 +0300

    cleared all unnecessary things

--- main.py
+++ main.py
@@ -2,18 +2,6 @@ import psutil
 from tkinter import *
 
 base_window = Tk()
-topFrame = Frame(base_window)
-topFrame.pack()
-bottomFrame = Frame(base_window)
-bottomFrame.pack(side=BOTTOM)
 
-button1 = Button(topFrame, text="Button 1", fg="grey")
-button2 = Button(topFrame, text="Button 2", fg="blue")
-button3 = Button(topFrame, text="Button 3", fg="red")
-button4 = Button(bottomFrame, text="Button 4", fg="black")
-button1.pack(side=TOP)
-button2.pack(side=LEFT)
-button3.pack(side=LEFT)
-button4.pack()
 base_window.mainloop()
 
